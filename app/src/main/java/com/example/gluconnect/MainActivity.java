05b3711666fb5d1311702594d73dcb4f75e8083b
Anniekobia <kobiaannunziata94@gmail.com>
2019-09-06 10:08:11 +0300

    Signin, signup and user information activities UI

---
+++ app/src/main/java/com/example/gluconnect/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.gluconnect;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    public void openRegisterActivity(View view) {
+        Intent intent = new Intent(this,RegisterActivity.class);
+        startActivity(intent);
+    }
+}
